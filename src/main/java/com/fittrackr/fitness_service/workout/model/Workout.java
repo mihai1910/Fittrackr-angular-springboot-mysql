10f75d97dba4e32f4d0a1687a5cf747e48597a70
mihai1910 <mihai1910@yahoo.com>
2025-12-12 10:40:38 +0200

    Configured MySQL database and created a model

---
+++ src/main/java/com/fittrackr/fitness_service/workout/model/Workout.java
@@ -0,0 +1,60 @@
+package com.fittrackr.fitness_service.workout.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+@Entity
+@Table (name= "workouts")
+public class Workout {
+
+        public Workout(){}
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @NotNull
+        @Enumerated(EnumType.STRING)
+        private WorkoutType type;
+
+        @Min(45)
+        private int durationMinutes;
+
+        @NotNull
+        private LocalDate date;
+
+        public void setId(Long id){
+                this.id = id;
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setType(WorkoutType type) {
+                this.type = type;
+        }
+
+        public WorkoutType getType() {
+                return type;
+        }
+
+        public void setDurationMinutes( int durationMinutes ){
+                this.durationMinutes = durationMinutes;
+        }
+
+        public int getDurationMinutes() {
+                return durationMinutes;
+        }
+
+        public void setDate(LocalDate date) {
+                this.date = date;
+        }
+
+        public LocalDate getDate() {
+                return date;
+        }
+}
---
+++ src/main/java/com/fittrackr/fitness_service/workout/model/WorkoutType.java
@@ -0,0 +1,5 @@
+package com.fittrackr.fitness_service.workout.model;
+
+public enum WorkoutType {
+    PUSH, PULL, LEGS
+}
